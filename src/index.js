afcea908b53df985cf533b3637fbada626f02d5c
krishna <krishnap@webmob.tech>
2019-08-14 12:08:20 +0530

    add state example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Clock from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
